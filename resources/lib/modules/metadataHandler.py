4d36aa380ad87e6332a8e91f13b16b49aa69d932
henryjfry <henry.j.fry@gmail.com>
2024-01-05 12:42:35 +0000

    small fix

--- resources/lib/modules/metadataHandler.py
+++ resources/lib/modules/metadataHandler.py
@@ -50,7 +50,7 @@ class MetadataHandler(object):
         self.season_landscape = g.get_bool_setting("season.landscape", "true")
         self.season_fanart = g.get_bool_setting("season.fanart", "true")
         self.episode_fanart = g.get_bool_setting("episode.fanart", "true")
-        self.episode_fanart = "false"
+        self.episode_fanart = False
         self.tvshows_preferred_art_source = g.get_int_setting(
             "tvshows.preferedsource", 1
         )
